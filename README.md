093b49ea989a2ac3dc596d6161df2576d9a0df8f
Michelle Poterek and Nathan Stewart <michelle+nathan@>
2017-06-12 10:58:53 -0700

    finish project

---
+++ README.md
@@ -0,0 +1,38 @@
+# Places Webpage
+
+#### Practice webpage for js objects, 06.12.2017
+
+#### Nathan Stewart and Michelle Poterek
+
+## Description
+
+This webpage is an Epicodus Intro to Programming exercise designed to practice with javascript objects and constructors. When the form is filled and submitted the location info will append to a list. When an li is clicked, the info contained within will display next to the form.
+
+## Setup/Installation
+
+Clone this repo, open index.html from folder into browser of choice.
+
+
+## Specifications
+
+
+This will display location name when form is submitted
+  Input: New York, Statue of Liberty, Summer, yaddayadda
+  Output: New York
+
+This will display information contained within each location next to the form
+  Input: (click) New York
+  Output: New York
+          Landmarks: Statue of Liberty
+          Time: Summer
+          Notes: yaddayadda
+
+## Technologies used
+
+HTML, CSS, Bootstrap, JQuery, javascript
+
+### License
+
+This project is licensed under the MIT license
+
+Copyright &copy; 2017 ** Nathan Stewart and Michelle Poterek**
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,41 @@
-function Location(place, landmarks, timeOfYear, notes) {
+function Location(place, landmarks, time, notes) {
   this.place = place;
   this.landmarks = landmarks;
-  this.timeOfYear = timeOfYear;
+  this.time = time;
   this.notes = notes;
-};
+}
+
+$(document).ready(function(){
+  $("form#new-location").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedPlace = $("input#new-place").val();
+    var inputtedLandmarks = $("input#new-landmarks").val();
+    var inputtedTime = $("input#new-time").val();
+    var inputtedNotes = $("input#new-notes").val();
+
+    var newLocation = new Location(inputtedPlace, inputtedLandmarks, inputtedTime, inputtedNotes);
+
+    $("ul#add-location").append("<li><span class='location'>" + newLocation.place + "</span></li>");
+
+    $(".location").last().click(function() {
+  $("#show-location").show();
+  $("#show-location h2").text(newLocation.place);
+  $(".landmarks").text(newLocation.landmarks);
+  $(".time").text(newLocation.time);
+  $(".notes").text(newLocation.notes);
+
+});
+
+    $("input#new-place").val("");
+    $("input#new-landmarks").val("");
+    $("input#new-time").val("");
+    $("input#new-notes").val("");
+
+
+
+
+
+
+  });
+});
